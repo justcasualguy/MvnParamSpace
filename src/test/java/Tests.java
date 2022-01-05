85c3cf1f67d9d7ffe62baa783e447d6024bf03b9
jakubChwesiuk <74013471+jakubChwesiuk@users.noreply.github.com>
2022-01-05 17:08:44 +0100

    Initial commit

---
+++ src/test/java/Tests.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    @Test
+    public void test(){
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");System.out.println("Test");
+        System.out.println("Test");
+
+
+
+    }
+}
